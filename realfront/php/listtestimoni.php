236f15f5df2a879b07ca39c064345c4fcef1cd53
kurniawan.umam <kurniawan.umam@pegadaian.co.id>
2021-06-23 15:30:03 +0700

    [dev_pea][front] : mengganti query

--- realfront/php/listtestimoni.php
+++ realfront/php/listtestimoni.php
@@ -7,7 +7,7 @@
 	header("Access-Control-Allow-Headers: x-requested-with, x-requested-by");
 	include("koneksi.php");
 	$data = array();
-	$cari = mysqli_query($koneksi, "SELECT * FROM tb_testimoni_pea where status_approve='1' ORDER BY RAND() ");
+	$cari = mysqli_query($koneksi, "SELECT * FROM tb_testimoni_pea where status_approve='1' ORDER BY id_testimoni DESC ");
     while ($row = mysqli_fetch_object($cari)) {
 	    $sql[] = $row;
 	}
